167d4239f3051460682deac8d378fd934935a4d6
Ishanka Lakshan Wasalage <isankalakshan@gmail.com>
2015-11-24 09:37:36 +0530

    Initial Commit

---
+++ Azure.mvc.Messenger.sln/Azure.mvc.Messenger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Azure.mvc.Messenger.MvcApplication" Language="C#" %>
---
+++ Azure.mvc.Messenger.sln/Azure.mvc.Messenger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Azure.mvc.Messenger.Startup))]
+namespace Azure.mvc.Messenger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
